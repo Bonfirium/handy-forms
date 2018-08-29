c656423d23adebbce9b5ffaf87f066e221a099ab
NickLatkovich <nicklatkovich@gmail.com>
2018-08-29 21:09:23 +0300

    Tests

--- test/test.js
+++ test/test.js
@@ -20,23 +20,31 @@ describe('without requirements', () => {
 		assert.deepStrictEqual(form.validate({}), {});
 	});
 	it('with several params', () => {
-		const fields = [1, 2, 3].map(i => `field_${i}`);
+		const fields = [1, 2, 3].map(i => ({
+			name: `field_${i}`,
+			value: Math.random(),
+		}));
 		const form = Form(
+			Field(fields[0].name),
 			Field('someField'),
-
-		)
-	})
+			Field(fields[1].name),
+			Field('someAnotherOneField'),
+			Field(fields[2].name),
+		);
+		const toValidate = fields.reduce((acc, { name, value }) => ({ ...acc, [name]: value }), {});
+		assert.deepStrictEqual(form.validate(toValidate), toValidate);
+	});
 });
 
-describe('', () => {
-	describe('with errors', () => {
+describe('required', () => {
+	describe('form errors', () => {
 		it('required field not provided', () => {
 			const fieldName = 'someRequiredField';
 			const form = Form(Field(fieldName).required());
 			try {
 				form.validate({});
 			} catch (err) {
-				assert.deepStrictEqual(err, { someRequiredField: ['required'] });
+				assert.deepStrictEqual(err, { [fieldName]: ['required'] });
 				return;
 			}
 			assert.fail();
@@ -48,6 +56,60 @@ describe('', () => {
 			const fieldValue = 123;
 			const form = Form(Field(fieldName).required());
 			assert.deepStrictEqual(form.validate({ [fieldName]: fieldValue }), { [fieldName]: fieldValue });
+		});
+	});
+});
+
+describe('string', () => {
+	describe('form errors', () => {
+		it('not a string', () => {
+			const fieldName = 'stringField';
+			const form = Form(Field(fieldName).string());
+			try {
+				form.validate({ [fieldName]: 123 });
+			} catch (err) {
+				assert.deepStrictEqual(err, { [fieldName]: ['must be a string'] });
+				return;
+			}
+			assert.fail();
+		});
+	});
+	describe('successful', () => {
+		it('put string', () => {
+			const fieldName = 'stringField';
+			const form = Form(Field(fieldName).string());
+			const fieldValue = 'someString';
+			const body = form.validate({ [fieldName]: fieldValue });
+			assert.deepStrictEqual(body, { [fieldName]: fieldValue });
+		});
+		it('create with two params "string"', () => {
+			const fieldName = 'someField';
+			const fieldValue = 'someString';
+			const form = Form(Field(fieldName).string().string());
+			const body = form.validate({ [fieldName]: fieldValue });
+			assert.deepStrictEqual(body, { [fieldName]: fieldValue });
+		});
+	});
+});
+
+describe('regexp', () => {
+	const fieldName = 'regexpField';
+	const form = Form(Field(fieldName).regexp(/^123$/));
+	describe('form errors', () => {
+		it('not a string', () => {
+			try {
+				form.validate({ [fieldName]: 123 });
+			} catch (err) {
+				assert.deepStrictEqual(err, { regexpField: ['must be a string'] });
+				return;
+			}
+			assert.fail();
+		});
+	});
+	describe('successful', () => {
+		it('successful form value', () => {
+			const body = form.validate({ [fieldName]: '123' });
+			assert.deepStrictEqual(body, { [fieldName]: '123' });
 		});
 	});
 });
